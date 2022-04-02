a9b9dde01f813c5c2f44b4dba432e2ce756debd3
shinghuey27 <shinghuey27@hotmail.com>
2022-04-02 19:17:13 +0800

    css update

--- src/components/header.js
+++ src/components/header.js
@@ -1,36 +1,31 @@
-import '../styles/header.css';
-
-
+import "../styles/header.css";
 
 const header = () => {
-    return (
-
-        <>
-        <div className='h-mainContainer'>
-
-            <div className='h-subContainer'>
-                <div>
-                    <div className='h-icon'>
-                    <img src={require('../Images/headerIcon.png')} />
-                    </div>
-                </div>
-                <div className='h-content'>
-                    <div> Savings Account-i </div>
-                    <div className='h-subcontent'> RM1,000,000.00 </div>
-                </div>
-
-                <div className='h-icon2'>
-                    <img src={require('../Images/acc.png')} />
-                </div>
-
-                <div className='h-icon3'>
-                 <img src={require('../Images/notification.png')} />
-                </div>
+  return (
+    <>
+      <div className="h-mainContainer">
+        <div className="h-subContainer">
+          <div className="abc">
+            <div className="h-icon">
+              <img src={require("../Images/headerIcon.png")} />
+            </div>
+            <div className="h-content">
+              <div> Savings Account-i </div>
+              <div className="h-subcontent"> RM1,000,000.00 </div>
+            </div>
+          </div>
+          <div className="test">
+            <div className="h-icon2">
+              <img src={require("../Images/acc.png")} />
             </div>
 
+            <div className="h-icon3">
+              <img src={require("../Images/notification.png")} />
+            </div>
+          </div>
         </div>
-
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 export default header;
