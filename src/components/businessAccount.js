d2864fab6aa9a7efc9f9d6f70a68a77ff0467612
JulieAnneRose96 <putri.juliana.roslan@gmail.com>
2022-04-03 15:58:08 +0800

    responsive for header, featured services & business Account

--- src/components/businessAccount.js
+++ src/components/businessAccount.js
@@ -17,15 +17,19 @@ const businessAccs = () => {
         </div>
 
             <div className="BA-subContainer">
-                <div>
-                    <div className='icons'>
-                        <div>01</div>
-                        </div>
+                <div className='BA-subContainer2'>
+                    <div>
+                        <div className='icons'>
+                            <div>01</div>
+                            </div>
+                    </div>
+                    <div className='contentContainer1'>
+                        <div> Savings Account-i </div>
+                        <div className='subcontentContainer1'> 1130 9340 0119 </div>
+                    </div>
+
                 </div>
-                <div className='contentContainer1'>
-                    <div> Savings Account-i </div>
-                    <div className='subcontentContainer1'> 1130 9340 0119 </div>
-                </div>
+              
 
                 <div className='contentContainer2'>
                     <div> Available Balance </div>
@@ -38,15 +42,18 @@ const businessAccs = () => {
             </div>
 
             <div className="BA-subContainer">
-                <div>
-                    <div className='icons'>
-                        <div>02</div> 
-                    </div>
+                <div className='BA-subContainer2'>
+                        <div>
+                            <div className='icons'>
+                                <div>02</div> 
+                            </div>
+                        </div>
+                        <div className='contentContainer1'>
+                            <div>SME First Account </div>
+                            <div className='subcontentContainer1'> 3101 2130 6725 </div>
+                        </div>
                 </div>
-                <div className='contentContainer1'>
-                    <div>SME First Account </div>
-                    <div className='subcontentContainer1'> 3101 2130 6725 </div>
-                </div>
+                
 
                 <div className='contentContainer2'>
                     <div> Available Balance </div>
@@ -59,15 +66,18 @@ const businessAccs = () => {
             </div>
 
             <div className="BA-subContainer">
-                <div>
-                    <div className='icons'>
-                    <div>03</div>
+                <div className='BA-subContainer2'>
+                    <div>
+                        <div className='icons'>
+                        <div>03</div>
+                        </div>
+                    </div>
+                    <div className='contentContainer1'>
+                        <div>Payment Account 02 </div>
+                        <div className='subcontentContainer1'> 3101 2130 6725 </div>
                     </div>
                 </div>
-                <div className='contentContainer1'>
-                    <div>Payment Account 02 </div>
-                    <div className='subcontentContainer1'> 3101 2130 6725 </div>
-                </div>
+                
 
                 <div className='contentContainer2'>
                     <div> Available Balance </div>
@@ -80,15 +90,18 @@ const businessAccs = () => {
             </div>
 
             <div className="BA-subContainer">
-                <div>
-                    <div className='icons'>
-                    <div>04</div>
-                    </div>
+                <div className='BA-subContainer2'>
+                        <div>
+                            <div className='icons'>
+                            <div>04</div>
+                            </div>
+                        </div>
+                        <div className='contentContainer1'>
+                            <div>Savings Account-i </div>
+                            <div className='subcontentContainer1'> 3101 2130 6725 </div>
+                        </div>
                 </div>
-                <div className='contentContainer1'>
-                    <div>Savings Account-i </div>
-                    <div className='subcontentContainer1'> 3101 2130 6725 </div>
-                </div>
+                
 
                 <div className='contentContainer2'>
                     <div> Available Balance </div>
@@ -101,121 +114,135 @@ const businessAccs = () => {
             </div>
 
             <div className="BA-subContainer">
-                <div>
-                    <div className='icons'>
-                        <div>05</div>
-                    </div>
-                </div>
-                <div className='contentContainer1'>
-                    <div>Savings Account-i </div>
-                    <div className='subcontentContainer1'> 3101 2130 6725 </div>
-                </div>
-
-                <div className='contentContainer2'>
-                    <div> Available Balance </div>
-                    <div  className='subcontentContainer2'> RM690,420.00 </div>
-                </div>
-
-                <div className='contentContainer3'>
-                    <div className='button'> More Actions </div>
-                </div>
-            </div>
-
-            <div className="BA-subContainer">
-                <div>
-                    <div className='icons'>
-                        <div>06</div>
-                    </div>
-                </div>
-                <div className='contentContainer1'>
-                    <div>Savings Account-i </div>
-                    <div className='subcontentContainer1'> 3101 2130 6725 </div>
-                </div>
-
-                <div className='contentContainer2'>
-                    <div> Available Balance </div>
-                    <div  className='subcontentContainer2'> RM690,420.00 </div>
-                </div>
-
-                <div className='contentContainer3'>
-                    <div className='button'> More Actions </div>
-                </div>
-            </div>
-
-            <div className="BA-subContainer">
-                <div>
-                    <div className='icons'>
-                        <div>07</div>
-                    </div>
-                </div>
-                <div className='contentContainer1'>
-                    <div>Savings Account-i </div>
-                    <div className='subcontentContainer1'> 3101 2130 6725 </div>
-                </div>
-
-                <div className='contentContainer2'>
-                    <div> Available Balance </div>
-                    <div  className='subcontentContainer2'> RM690,420.00 </div>
-                </div>
-
-                <div className='contentContainer3'>
-                    <div className='button'> More Actions </div>
-                </div>
-            </div>
-
-            <div className="BA-subContainer">
-                <div>
-                    <div className='icons'>
-                        <div>08</div>
-                    </div>
-                </div>
-                <div className='contentContainer1'>
-                    <div>Savings Account-i </div>
-                    <div className='subcontentContainer1'> 3101 2130 6725 </div>
-                </div>
-
-                <div className='contentContainer2'>
-                    <div> Available Balance </div>
-                    <div  className='subcontentContainer2'> RM690,420.00 </div>
-                </div>
-
-                <div className='contentContainer3'>
-                    <div className='button'> More Actions </div>
-                </div>
-            </div>
-
-            <div className="BA-subContainer">
-                <div>
-                    <div className='icons'>
-                        <div>09</div>
-                    </div>
-                </div>
-                <div className='contentContainer1'>
-                    <div>Savings Account-i </div>
-                    <div className='subcontentContainer1'> 3101 2130 6725 </div>
-                </div>
-
-                <div className='contentContainer2'>
-                    <div> Available Balance </div>
-                    <div  className='subcontentContainer2'> RM690,420.00 </div>
-                </div>
-
-                <div className='contentContainer3'>
-                    <div className='button'> More Actions </div>
-                </div>
-            </div>
-
-            <div className="BA-subContainer">
-                <div>
-                    <div className='icons'>
-                        <div>10</div>
+                <div className='BA-subContainer2'>
+                        <div>
+                            <div className='icons'>
+                                <div>05</div>
+                            </div>
+                        </div>
+                        <div className='contentContainer1'>
+                            <div>Savings Account-i </div>
+                            <div className='subcontentContainer1'> 3101 2130 6725 </div>
+                        </div>
                     </div>
                 
+
+                <div className='contentContainer2'>
+                    <div> Available Balance </div>
+                    <div  className='subcontentContainer2'> RM690,420.00 </div>
                 </div>
-                <div className='contentContainer1'>
-                    <div>Savings Account-i </div>
-                    <div className='subcontentContainer1'> 3101 2130 6725 </div>
+
+                <div className='contentContainer3'>
+                    <div className='button'> More Actions </div>
                 </div>
+            </div>
+
+            <div className="BA-subContainer">
+                <div className='BA-subContainer2'>
+                    <div>
+                        <div className='icons'>
+                            <div>06</div>
+                        </div>
+                    </div>
+                    <div className='contentContainer1'>
+                        <div>Savings Account-i </div>
+                        <div className='subcontentContainer1'> 3101 2130 6725 </div>
+                    </div>
+                        </div>
+                
+
+                <div className='contentContainer2'>
+                    <div> Available Balance </div>
+                    <div  className='subcontentContainer2'> RM690,420.00 </div>
+                </div>
+
+                <div className='contentContainer3'>
+                    <div className='button'> More Actions </div>
+                </div>
+            </div>
+
+            <div className="BA-subContainer">
+                <div className='BA-subContainer2'>
+                    <div>
+                        <div className='icons'>
+                            <div>07</div>
+                        </div>
+                    </div>
+                    <div className='contentContainer1'>
+                        <div>Savings Account-i </div>
+                        <div className='subcontentContainer1'> 3101 2130 6725 </div>
+                    </div>
+                        </div>
+                
+
+                <div className='contentContainer2'>
+                    <div> Available Balance </div>
+                    <div  className='subcontentContainer2'> RM690,420.00 </div>
+                </div>
+
+                <div className='contentContainer3'>
+                    <div className='button'> More Actions </div>
+                </div>
+            </div>
+            <div className="BA-subContainer">
+                <div className='BA-subContainer2'>
+                    <div>
+                        <div className='icons'>
+                            <div>08</div>
+                        </div>
+                    </div>
+                    <div className='contentContainer1'>
+                        <div>Savings Account-i </div>
+                        <div className='subcontentContainer1'> 3101 2130 6725 </div>
+                    </div>
+                        </div>
+                
+
+                <div className='contentContainer2'>
+                    <div> Available Balance </div>
+                    <div  className='subcontentContainer2'> RM690,420.00 </div>
+                </div>
+
+                <div className='contentContainer3'>
+                    <div className='button'> More Actions </div>
+                </div>
+            </div>
+            <div className="BA-subContainer">
+                <div className='BA-subContainer2'>
+                    <div>
+                        <div className='icons'>
+                            <div>09</div>
+                        </div>
+                    </div>
+                    <div className='contentContainer1'>
+                        <div>Savings Account-i </div>
+                        <div className='subcontentContainer1'> 3101 2130 6725 </div>
+                    </div>
+                        </div>
+                
+
+                <div className='contentContainer2'>
+                    <div> Available Balance </div>
+                    <div  className='subcontentContainer2'> RM690,420.00 </div>
+                </div>
+
+                <div className='contentContainer3'>
+                    <div className='button'> More Actions </div>
+                </div>
+            </div>
+            <div className="BA-subContainer">
+                <div className='BA-subContainer2'>
+                    <div>
+                        <div className='icons'>
+                            <div>10</div>
+                        </div>
+                    </div>
+                    <div className='contentContainer1'>
+                        <div>Savings Account-i </div>
+                        <div className='subcontentContainer1'> 3101 2130 6725 </div>
+                    </div>
+                        </div>
+                
 
                 <div className='contentContainer2'>
                     <div> Available Balance </div>
